78cecb469d1760787ca2a190951b40a544608367
Ole Morten Didriksen <code@oledid.com>
2017-08-11 21:19:31 +0200

    IEnumerableExtensions.Distinct(this, selectorFunc)

---
+++ src/SyntaxImprovement/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oledid.SyntaxImprovement
+{
+	// ReSharper disable once InconsistentNaming
+	public static class IEnumerableExtensions
+	{
+		public static IEnumerable<TSource> Distinct<TKey, TSource>(this IEnumerable<TSource> collection, Func<TSource, TKey> selector)
+		{
+			var hashSet = new HashSet<TKey>();
+			foreach (var element in collection)
+			{
+				var value = selector.Invoke(element);
+				if (hashSet.Contains(value))
+				{
+					continue;
+				}
+				hashSet.Add(value);
+				yield return element;
+			}
+		}
+	}
+}

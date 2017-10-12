b30f9d1ac8388e09dcf2b4baf6c08c7c3dcf3dd3
Ole Morten Didriksen <code@oledid.com>
2017-10-12 14:17:09 +0200

    decimal.RoundUp, decimal.RoundDown

---
+++ src/SyntaxImprovement.Tests/Extensions/DecimalExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Extensions
+{
+	public class DecimalExtensionsTests
+	{
+		[Fact]
+		public void TheRoundDownMethod()
+		{
+			const decimal value = 0.123456789m;
+
+			Assert.NotEqual(0.12345m, value);
+
+			Assert.Equal(0m, value.RoundDown(0));
+
+			Assert.Equal(0.12345m, value.RoundDown(5));
+
+			Assert.Equal(0.123456m, value.RoundDown(6));
+		}
+
+		[Fact]
+		public void TheRoundUpMethod()
+		{
+			const decimal value = 0.123456789m;
+
+			Assert.NotEqual(0.12345m, value);
+
+			Assert.Equal(1m, value.RoundUp(0));
+
+			Assert.Equal(0.124m, value.RoundUp(3));
+
+			Assert.Equal(0.123457m, value.RoundUp(6));
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Extensions/DecimalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace oledid.SyntaxImprovement
+{
+	public static class DecimalExtensions
+	{
+		public static decimal RoundDown(this decimal value, double numberOfDecimals)
+		{
+			var power = Convert.ToDecimal(Math.Pow(10, numberOfDecimals));
+			return Math.Floor(value * power) / power;
+		}
+
+		public static decimal RoundUp(this decimal value, double numberOfDecimals)
+		{
+			var power = Convert.ToDecimal(Math.Pow(10, numberOfDecimals));
+			return Math.Ceiling(value * power) / power;
+		}
+	}
+}

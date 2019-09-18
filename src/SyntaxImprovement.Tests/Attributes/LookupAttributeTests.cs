68261bf5d3a022927e36ff9ff5b8111b187b8b06
Ole Morten Didriksen <code@oledid.com>
2019-09-18 15:38:28 +0200

    LookupAttribute

---
+++ src/SyntaxImprovement.Tests/Attributes/LookupAttributeTests.cs
@@ -0,0 +1,58 @@
+﻿using oledid.SyntaxImprovement.Attributes;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Attributes
+{
+	public class LookupAttributeTests
+	{
+		public enum TestEnum
+		{
+			[Lookup("A", "1")]
+			ItemWithOneValue,
+
+			[Lookup("A", "1")]
+			[Lookup("B", "2")]
+			ItemWithTwoValues,
+
+			ItemWithoutValues
+		}
+
+		[Fact]
+		public void Can_get_stored_values()
+		{
+			{
+				var expected = "1";
+				var actual = LookupAttribute.GetValueFromAttribute(TestEnum.ItemWithOneValue, "A");
+				Assert.Equal(expected, actual);
+			}
+
+			{
+				var expected = "1";
+				var actual = LookupAttribute.GetValueFromAttribute(TestEnum.ItemWithTwoValues, "A");
+				Assert.Equal(expected, actual);
+			}
+
+			{
+				var expected = "2";
+				var actual = LookupAttribute.GetValueFromAttribute(TestEnum.ItemWithTwoValues, "B");
+				Assert.Equal(expected, actual);
+			}
+		}
+
+		[Fact]
+		public void Returns_null_if_no_values_stored()
+		{
+			{
+				string expected = null;
+				var actual = LookupAttribute.GetValueFromAttribute(TestEnum.ItemWithOneValue, "B");
+				Assert.Equal(expected, actual);
+			}
+
+			{
+				string expected = null;
+				var actual = LookupAttribute.GetValueFromAttribute(TestEnum.ItemWithoutValues, "C");
+				Assert.Equal(expected, actual);
+			}
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Attributes/LookupAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace oledid.SyntaxImprovement.Attributes
+{
+	/// <summary>
+	/// Add keyValuePairs to enum-values, access them with LookupAttribute.GetValueFromAttribute(EnumType.Value, key)
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+	public class LookupAttribute : Attribute
+	{
+		public string Key { get; }
+		public string Value { get; }
+
+		public LookupAttribute(string key, string value)
+		{
+			Key = key;
+			Value = value;
+		}
+
+		public static string GetValueFromAttribute<T>(T instance, string key) => GetValueFromAttribute(typeof(T), instance, key);
+
+		public static string GetValueFromAttribute(Type type, object instance, string key)
+		{
+			var memberInfos = type.GetMember(instance.ToString());
+			if (memberInfos.Length == 0)
+			{
+				return string.Empty;
+			}
+
+			var memberInfo = memberInfos[0];
+			var attributeValues = memberInfo.GetCustomAttributes(typeof(LookupAttribute), false);
+
+			return attributeValues
+				.Cast<LookupAttribute>()
+				.Where(attr => attr.Key == key).FirstOrDefault()?.Value;
+		}
+	}
+}

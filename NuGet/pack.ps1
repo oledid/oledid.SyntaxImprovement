760d4312931f92f4361b8257fc2f58dcf7bc186c
Ole Morten Didriksen <code@oledid.com>
2019-09-25 14:23:40 +0200

    Fix

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,7 +1,7 @@
 $thisPath = $MyInvocation.MyCommand.Definition
 $root = Resolve-Path "$thisPath/../.."
 $version = [System.Reflection.Assembly]::LoadFile("$root/src/SyntaxImprovement/bin/Release/netstandard2.0/oledid.SyntaxImprovement.dll").GetName().Version
-$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+$versionStr = "{0}.{1}.{2}-beta" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
--- update-assemblyversion.ps1
+++ update-assemblyversion.ps1
@@ -4,17 +4,22 @@ $assemblyFiles = Get-ChildItem -Recurse . AssemblyInfo.cs
 $versionTxt = Get-Content version.txt -First 1
 $splitVersion = ($versionTxt -replace "\+", ".").split(".")
 
-$majorVersion = $splitVersion[0]
-$minorVersion = $splitVersion[1]
-$buildVersion = $splitVersion[2]
 $buildCounter = $splitVersion[3]
+if ([string]::IsNullOrWhitespace($buildCounter)) {
+	$buildCounter = "0"
+}
+
+$major = "0"
+$minor = "4"
+$build = $buildCounter
+$revision = "0"
 
 foreach ($file in $assemblyFiles) {
 	(Get-Content $file.PSPath) | ForEach-Object {
 		if ($_ -match $pattern) {
 			# We have found the matching line
 			# Edit the version number and put back.
-			$newVersion = "{0}.{1}.{2}.{3}" -f $majorVersion, $minorVersion, $buildVersion, $buildCounter
+			$newVersion = "{0}.{1}.{2}.{3}" -f $major, $minor, $build, $revision
 			write-host "Setting AssemblyInfo version to $newVersion"
 			'[assembly: AssemblyVersion("{0}")]' -f $newVersion
 		}

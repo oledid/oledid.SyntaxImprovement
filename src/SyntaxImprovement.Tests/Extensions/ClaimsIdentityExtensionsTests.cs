e4534e680e392df522b0aa63d18ff0ed401e1309
Ole Morten Didriksen <code@oledid.com>
2019-09-28 00:11:08 +0200

    Add ClaimsIdentityExtensions, change TargetFramework to netstandard2.1, bump minor version

---
+++ src/SyntaxImprovement.Tests/Extensions/ClaimsIdentityExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using oledid.SyntaxImprovement.Extensions;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Extensions
+{
+	public class ClaimsIdentityExtensionsTests
+	{
+		[Fact]
+		public void It_adds_or_updates_a_string_claim()
+		{
+			var ci1 = new ClaimsIdentity();
+
+			ci1.AddClaim(new Claim("a", "1"));
+			Assert.Equal("1", ci1.FindFirst(c => c.Type == "a").Value);
+			ci1.SetClaim("a", "2");
+			Assert.Equal("2", ci1.FindFirst(c => c.Type == "a").Value);
+
+			Assert.Null(ci1.FindFirst(c => c.Type == "b"));
+			ci1.SetClaim("b", "notnull");
+			Assert.NotNull(ci1.FindFirst(c => c.Type == "b"));
+			Assert.Equal("notnull", ci1.FindFirst(c => c.Type == "b").Value);
+		}
+
+		[Fact]
+		public void It_adds_or_updates_a_bool_claim()
+		{
+			var ci1 = new ClaimsIdentity();
+
+			ci1.AddClaim(new Claim("a", false.ToString()));
+			Assert.Equal(false.ToString(), ci1.FindFirst(c => c.Type == "a").Value);
+			ci1.SetClaim("a", true);
+			Assert.Equal(true.ToString(), ci1.FindFirst(c => c.Type == "a").Value);
+
+			Assert.Null(ci1.FindFirst(c => c.Type == "b"));
+			ci1.SetClaim("b", true);
+			Assert.NotNull(ci1.FindFirst(c => c.Type == "b"));
+			Assert.Equal(true.ToString(), ci1.FindFirst(c => c.Type == "b").Value);
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Extensions/ClaimsIdentityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+
+namespace oledid.SyntaxImprovement.Extensions
+{
+	public static class ClaimsIdentityExtensions
+	{
+		public static void SetClaim(this ClaimsIdentity identity, string claimType, string value)
+		{
+			if (identity.HasClaim(c => c.Type == claimType))
+			{
+				identity.RemoveClaim(identity.FindFirst(c => c.Type == claimType));
+			}
+			identity.AddClaim(new Claim(claimType, value));
+		}
+
+		public static void SetClaim(this ClaimsIdentity identity, string claimType, bool value)
+		{
+			if (identity.HasClaim(c => c.Type == claimType))
+			{
+				identity.RemoveClaim(identity.FindFirst(c => c.Type == claimType));
+			}
+			if (value)
+			{
+				identity.AddClaim(new Claim(claimType, value.ToString()));
+			}
+		}
+	}
+}

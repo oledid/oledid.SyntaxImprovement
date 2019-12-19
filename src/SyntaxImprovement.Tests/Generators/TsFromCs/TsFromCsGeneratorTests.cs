42407b382115ecbe7839d382f995e146c70dd532
Ole Morten Didriksen <code@oledid.com>
2019-12-19 14:31:26 +0100

    Work on TsFromCsGenerator

---
+++ src/SyntaxImprovement.Tests/Generators/TsFromCs/TsFromCsGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using oledid.SyntaxImprovement.Generators.TsFromCs;
+using System;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Generators.TsFromCs
+{
+	public class TsFromCsGeneratorTests
+	{
+		[Fact]
+		public void It_can_generate_dtos()
+		{
+			var result = TsFromCsGenerator.GenerateTypescriptInterfaceFromCsharpClass(typeof(PersonEntity), typeof(TypesEntity));
+			Assert.Equal("", result);
+		}
+
+		public class PersonEntity
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public bool IsActive { get; set; }
+			public Guid UniqueId { get; set; }
+			public DateTime CreatedOn { get; set; }
+			public DateTime? DeletedOn { get; set; }
+
+			public TypesEntity FavoriteTypes { get; set; }
+			public PersonEntity Parent { get; set; }
+		}
+
+		public class TypesEntity
+		{
+			public int Int { get; set; }
+			public int? NullableInt { get; set; }
+			public bool? NullableBool { get; set; }
+			public long Long { get; set; }
+			public long? NullableLong { get; set; }
+			public short Short { get; set; }
+			public short? NullableShort { get; set; }
+			public string String { get; set; }
+			public Guid? NullableGuid { get; set; }
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Generators/TsFromCs/TsFromCsGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace oledid.SyntaxImprovement.Generators.TsFromCs
+{
+	public class TsFromCsGenerator
+	{
+		public static string GenerateTypescriptInterfaceFromCsharpClass(params Type[] types)
+		{
+			var stringBuilder = new StringBuilder();
+
+			foreach (var type in types)
+			{
+				var typeName = type.Name;
+				var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+				stringBuilder
+					.Append($"interface I{typeName} {{\r\n");
+
+				foreach (var property in properties)
+				{
+					var isNullable = Nullable.GetUnderlyingType(property.PropertyType) != null || property.PropertyType == typeof(string) || types.Contains(property.PropertyType);
+
+					stringBuilder
+						.Append('\t')
+						.Append(property.Name.ToLowerCamelCase())
+						.Append(isNullable ? "?" : string.Empty)
+						.Append(':')
+						.Append(' ')
+						.Append(GetTsType(property.PropertyType))
+						.Append(';')
+						.Append(Environment.NewLine);
+				}
+
+				stringBuilder
+					.Append('}')
+					.Append(Environment.NewLine);
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		private static string GetTsType(Type type)
+		{
+			var @switch = new Dictionary<Type, Func<string>>
+			{
+				{ typeof(bool), () => "boolean" },
+				{ typeof(char), () => "string" },
+				{ typeof(string), () => "string" },
+				{ typeof(Guid), () => "string" },
+				{ typeof(short), () => "number" },
+				{ typeof(int), () => "number" },
+				{ typeof(long), () => "number" },
+				{ typeof(decimal), () => "number" },
+				{ typeof(DateTime), () => "Date" },
+			};
+
+			var tsType = @switch.GetValueOrNull(type)?.Invoke();
+
+			if (type.IsArray)
+			{
+				var arrayType = type.GetElementType();
+				return "Array<" + GetTsType(arrayType) + ">";
+			}
+
+			if (type.GetInterfaces().Any(@interface => @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IDictionary<,>)))
+			{
+				var argTypes = type.GetGenericArguments();
+				var keyType = GetTsType(argTypes[0]);
+				var valueType = GetTsType(argTypes[1]);
+				return $"{{ [key: {keyType}]: {valueType} }}";
+			}
+
+			if (typeof(System.Collections.IEnumerable).IsAssignableFrom(type))
+			{
+				var collectionType = type.GetGenericArguments()[0];
+				return "Array<" + GetTsType(collectionType) + ">";
+			}
+
+			if (Nullable.GetUnderlyingType(type) != null)
+			{
+				return GetTsType(Nullable.GetUnderlyingType(type));
+			}
+
+			if (type.IsEnum)
+			{
+				return "number";
+			}
+
+			return tsType ?? "any";
+		}
+	}
+}

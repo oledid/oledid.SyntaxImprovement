16fbca485137b0e7379efc77a7bfc85250cc1879
Ole Morten Didriksen <code@oledid.com>
2019-12-20 11:16:41 +0100

    Add @jbtule 's AESThenHMAC encryption

---
+++ src/SyntaxImprovement.Tests/Algorithms/AESThenHMACTests.cs
@@ -0,0 +1,29 @@
+﻿using oledid.SyntaxImprovement.Algorithms.Cryptography;
+using System;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Algorithms
+{
+	public class AESThenHMACTests
+	{
+		[Fact]
+		public static void It_works()
+		{
+			{
+				var secretMessage = "Cryptography is hard.";
+				var password = Guid.NewGuid().ToString();
+				var encrypted = AESThenHMAC.SimpleEncryptWithPassword(secretMessage, password);
+				var decrypted = AESThenHMAC.SimpleDecryptWithPassword(encrypted, password);
+				Assert.Equal(secretMessage, decrypted);
+			}
+
+			{
+				var secretMessage = "Cryptography is hard.";
+				var password = Guid.NewGuid().ToString();
+				var encrypted = AESThenHMAC.SimpleEncryptWithPassword(secretMessage, password);
+				var decrypted = AESThenHMAC.SimpleDecryptWithPassword(encrypted, "wrong password");
+				Assert.NotEqual(secretMessage, decrypted);
+			}
+		}
+	}
+}

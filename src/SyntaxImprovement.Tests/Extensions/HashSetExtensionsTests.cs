ed67bca687ae9904704df3967a7ba6cfcff48a53
Ole Morten Didriksen <code@oledid.com>
2020-02-17 14:00:56 +0100

    HashSetExtensions, AddRange

---
+++ src/SyntaxImprovement.Tests/Extensions/HashSetExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using oledid.SyntaxImprovement.Extensions;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Extensions
+{
+	public class HashSetExtensionsTests
+	{
+		[Fact]
+		public void It_defines_AddRange()
+		{
+			var hashSet = new HashSet<int>();
+			hashSet.Add(3);
+			hashSet.AddRange(new[] { 3, 2, 1 });
+			Assert.Equal(3, hashSet.Count);
+			Assert.Contains(1, hashSet);
+			Assert.Contains(2, hashSet);
+			Assert.Contains(3, hashSet);
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Extensions/HashSetExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace oledid.SyntaxImprovement.Extensions
+{
+	public static class HashSetExtensions
+	{
+		public static void AddRange<T>(this HashSet<T> hashSet, IEnumerable<T> elements)
+		{
+			foreach (var element in elements)
+			{
+				hashSet.Add(element);
+			}
+		}
+	}
+}

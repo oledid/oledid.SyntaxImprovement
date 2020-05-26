1e6ec809fd2b0c8f8e0f11ba729cdcdfa73f14ee
Ole Morten Didriksen <code@oledid.com>
2020-05-26 19:57:36 +0200

    oledid.SyntaxImprovement.Reflection.ObjectComparer.GetParametersWithValueDifference

---
+++ src/SyntaxImprovement.Tests/Reflection/ObjectComparerTests.cs
@@ -0,0 +1,56 @@
+﻿using oledid.SyntaxImprovement.Reflection;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Reflection
+{
+	public class ObjectComparerTests
+	{
+		public class GetParametersWithValueDifferenceTests
+		{
+			public void It_detects_differences()
+			{
+				var a = new TestModelOne
+				{
+					Id = 1,
+					Name = "A"
+				};
+
+				var b = new TestModelOne
+				{
+					Id = 2,
+					Name = "B"
+				};
+
+				var comparison = ObjectComparer.GetParametersWithValueDifference(a, b);
+				Assert.Contains(comparison, item => item.Property.Name == nameof(TestModelOne.Id));
+				Assert.Contains(comparison, item => item.Property.Name == nameof(TestModelOne.Name));
+				Assert.Equal(2, comparison.Count);
+			}
+
+			public void It_ignores_equal_fields()
+			{
+				var a = new TestModelOne
+				{
+					Id = 1,
+					Name = "One"
+				};
+
+				var b = new TestModelOne
+				{
+					Id = 2,
+					Name = "One"
+				};
+
+				var comparison = ObjectComparer.GetParametersWithValueDifference(a, b);
+				Assert.Contains(comparison, item => item.Property.Name == nameof(TestModelOne.Id));
+				Assert.Single(comparison);
+			}
+
+			public class TestModelOne
+			{
+				public int Id { get; set; }
+				public string Name { get; set; }
+			}
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Reflection/ObjectComparer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace oledid.SyntaxImprovement.Reflection
+{
+	public static class ObjectComparer
+	{
+		public static List<PropertyWithValueDifference<TObject>> GetParametersWithValueDifference<TObject>(TObject a, TObject b, BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+		{
+			var fieldsWithDiff = new List<PropertyWithValueDifference<TObject>>();
+			foreach (var property in typeof(TObject).GetProperties(bindingFlags))
+			{
+				var aValue = property.GetValue(a);
+				var bValue = property.GetValue(b);
+
+				if (aValue.Equals(bValue))
+				{
+					fieldsWithDiff.Add(new PropertyWithValueDifference<TObject>
+					{
+						Property = property,
+						A = a,
+						B = b,
+						AValue = aValue,
+						BValue = bValue
+					});
+				}
+			}
+
+			return fieldsWithDiff;
+		}
+
+		public class PropertyWithValueDifference<TObject>
+		{
+			public PropertyInfo Property { get; set; }
+			public TObject A { get; set; }
+			public TObject B { get; set; }
+			public object AValue { get; set; }
+			public object BValue { get; set; }
+		}
+	}
+}

5cc287a59d4c2c1ace93a58450a29ddd36c38f09
Ole Morten Didriksen <code@oledid.com>
2020-12-23 10:40:17 +0100

    CLSCompliant

--- src/SyntaxImprovement/Properties/AssemblyInfo.cs
+++ src/SyntaxImprovement/Properties/AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Ole Morten Didriksen 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: System.CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from

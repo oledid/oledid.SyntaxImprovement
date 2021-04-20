8ca0e31356ed9e175020619cfdd1bca72689a197
Ole Morten Didriksen <code@oledid.com>
2021-04-20 18:13:44 +0200

    ConcurrentMemoryCache

---
+++ src/SyntaxImprovement.Tests/Caching/ConcurrentMemoryCacheTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using oledid.SyntaxImprovement.Caching;
+using Xunit;
+
+namespace oledid.SyntaxImprovement.Tests.Caching
+{
+	public class ConcurrentMemoryCacheTests
+	{
+		[Fact]
+		public void It_works()
+		{
+			var memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+			var wrapper = new ConcurrentMemoryCache(memoryCache, 5 * 60);
+
+			var counter = 0;
+
+			var operations = new List<Action>();
+			for (var i = 0; i < 10000; ++i)
+			{
+				operations.Add(() => wrapper.GetOrCreate("ConcurrentMemoryCacheTests.It_works", () => counter += 1));
+			}
+
+			Parallel.Invoke(operations.ToArray());
+
+			Assert.Equal(1, counter);
+		}
+	}
+}
---
+++ src/SyntaxImprovement/Caching/ConcurrentMemoryCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using oledid.SyntaxImprovement.Threading;
+
+namespace oledid.SyntaxImprovement.Caching
+{
+	public class ConcurrentMemoryCache
+	{
+		private readonly IMemoryCache MemoryCache;
+		private readonly int CacheDurationInSeconds;
+
+		public ConcurrentMemoryCache(IMemoryCache memoryCache, int cacheDurationInSeconds)
+		{
+			MemoryCache = memoryCache;
+			CacheDurationInSeconds = cacheDurationInSeconds;
+		}
+
+		public T GetOrCreate<T>(string cacheKey, Func<T> constructItem)
+		{
+			// ReSharper disable once InconsistentlySynchronizedField
+			if (MemoryCache.TryGetValue(cacheKey, out var cacheItem1))
+			{
+				return (T)cacheItem1;
+			}
+
+			lock (LockObjectProvider.GetOrCreateLockObjectFromCallerInformation(additionalKeyParts: new object[]{ cacheKey }))
+			{
+				if (MemoryCache.TryGetValue(cacheKey, out var cacheItem2))
+				{
+					return (T)cacheItem2;
+				}
+
+				var item = constructItem.Invoke();
+				_ = MemoryCache.Set(cacheKey, item, absoluteExpirationRelativeToNow: TimeSpan.FromSeconds(CacheDurationInSeconds));
+				return item;
+			}
+		}
+	}
+}

6b124537f76bbfa6dd69286f383e63ff62148adc
Ole Morten Didriksen <code@oledid.com>
2025-04-24 12:55:50 +0200

    retry

--- src/SyntaxImprovement.Tests/Generators/TsFromCs/InterfaceGeneratorTests.cs
+++ src/SyntaxImprovement.Tests/Generators/TsFromCs/InterfaceGeneratorTests.cs
@@ -29,7 +29,7 @@ export interface IPersonEntity {
 ";
 
 			var actual = TsFromCsGenerator.GenerateTypescriptInterfaceFromCsharpClass(typeof(IHub), typeof(PersonEntity));
-			Assert.Equal(expected.RegexReplace("\\s+", " ", System.Text.RegularExpressions.RegexOptions.Compiled), actual.RegexReplace("\\s+", " ", System.Text.RegularExpressions.RegexOptions.Compiled));
+			Assert.Equal(expected.Replace("\r", ""), actual.Replace("\r", ""));
 		}
 
 		public interface IHub
